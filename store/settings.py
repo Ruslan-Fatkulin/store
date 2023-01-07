0e11c540d179265c9f461807174a9b56667390a8
Ruslan-Fatkulin <aka.ruslanjon25@gmail.com>
2023-01-07 23:24:05 +0500

    added postgresql

--- store/settings.py
+++ store/settings.py
@@ -73,11 +73,16 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'dvdrental',
+       'USER': 'postgres',
+       'PASSWORD': 'FORTA',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
